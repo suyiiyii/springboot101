8e91e2c61ff0b6291ed5c5ec18b888f8dad12fc7
suyiiyii <suyiiyii@gmail.com>
2024-05-30 15:09:12 +0800

    feat: 实现server_info接口

--- src/main/java/top/suyiiyii/MyApplication.java
+++ src/main/java/top/suyiiyii/MyApplication.java
@@ -1,21 +1,42 @@
 package top.suyiiyii;
 
+import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @SpringBootApplication
 public class MyApplication {
 
-    @RequestMapping("/")
-    String home() {
-        return "Hellllllllo";
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(MyApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    Map home() {
+        Map<String, String> env = System.getenv();
+        return env;
+    }
+
+    @RequestMapping("/health")
+    String health() {
+        return "OK";
+    }
+
+    @RequestMapping("/server_info")
+    ServerInfo serverInfo() {
+        ServerInfo info = new ServerInfo();
+        info.setMessage("SpringBoot101 server by suyiiyii.");
+        return info;
+    }
+
+    @Data
+    public static class ServerInfo {
+        private String message;
     }
 
 }
